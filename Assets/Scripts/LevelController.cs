38d500c54daf07b5dd1b352378522c305e50cab1
xlab <alex22021999@mail.ru>
2024-06-27 11:30:34 +0300

    Final

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Golf
+{
+    public class LevelController : MonoBehaviour
+    {
+        public SpawnerStone spawner;
+        public float delayMax = 2f;
+        public float delayMin = 0.5f;
+        public float delayStep = 0.1f;
+
+        private float m_delay = 0.5f;
+
+        private float m_lastSpawnedTime = 0;
+
+        public int score = 0;
+        public int hightScore = 0;
+
+        private List<GameObject> m_stones = new List<GameObject>(16);
+
+        private void Start()
+        {
+            m_lastSpawnedTime = Time.time;
+            RefreshDelay();
+        }
+
+        private void OnStickHit()
+        {
+            score++;
+            hightScore = Mathf.Max(hightScore, score);
+            Debug.Log($"score: {score} - hightScore: {hightScore}");
+        }
+
+        private void OnEnable()
+        {
+            GameEvents.onStickHit += OnStickHit;
+            score = 0;
+        }
+
+        private void OnDisable()
+        {
+            GameEvents.onStickHit -= OnStickHit;
+        }
+
+        private void GameOver()
+        {
+           Debug.Log("Game OVER!!!");
+           enabled = false;
+        }
+
+        public void ClearStones()
+        {
+            foreach (var stone in m_stones)
+            { 
+                Destroy(stone);
+            }
+            m_stones.Clear();
+        }
+
+        public void RefreshDelay()
+        {
+            m_delay = UnityEngine.Random.Range(delayMin, delayMax);
+            delayMax = Mathf.Max(delayMin, delayMax - delayStep);
+        }
+
+        private void Update()
+        {
+                if (Time.time >= m_lastSpawnedTime + m_delay)
+                { 
+                    var stone = spawner.Spawn();
+                    m_stones.Add(stone);
+                    m_lastSpawnedTime = Time.time;
+
+                    RefreshDelay();
+                }
+        }
+    }
+}
